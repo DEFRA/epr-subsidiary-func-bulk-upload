1a2923994eb3fb60525d80e34a93084707cdee7f
BrianKnightEviden <155995473+BrianKnightEviden@users.noreply.github.com>
2024-07-31 16:52:22 +0100

    Added basic function for bulk upload

---
+++ src/EPR.SubsidiaryBulkUploadFunction/BulkUploadFunction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace EPR.SubsidiaryBulkUploadFunction
+{
+    public class BulkUploadFunction
+    {
+        private readonly ILogger<BulkUploadFunction> _logger;
+
+        public BulkUploadFunction(ILogger<BulkUploadFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(BulkUploadFunction))]
+        public async Task Run([BlobTrigger("samples-workitems/{name}", Connection = "SubsidiaryBlobStorageConnectionString")] Stream stream, string name)
+        {
+            using var blobStreamReader = new StreamReader(stream);
+            var content = await blobStreamReader.ReadToEndAsync();
+            _logger.LogInformation("C# Blob trigger function Processed blob\n Name: {Name} \n Data: {Content}", name, content);
+        }
+    }
+}
