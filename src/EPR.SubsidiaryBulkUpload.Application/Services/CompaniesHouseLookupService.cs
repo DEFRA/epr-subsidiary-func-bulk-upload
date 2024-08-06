c154c84979c74843d8fa543f0d8ed1b05c93da5e
Abid Kayani <abid.kayani.external@eviden.com>
2024-08-06 16:23:46 +0100

    416704: updates on Az func, services

---
+++ src/EPR.SubsidiaryBulkUpload.Application/Services/CompaniesHouseLookupService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using EPR.SubsidiaryBulkUpload.Application.Models;
+using EPR.SubsidiaryBulkUpload.Application.Services.Interfaces;
+
+namespace FrontendSchemeRegistration.Application.Services;
+public class CompaniesHouseLookupService : ICompaniesHouseLookupService
+{
+    private const string CompaniesHouseEndpoint = "CompaniesHouse/companies";
+
+    private readonly HttpClient _httpClient;
+
+    public CompaniesHouseLookupService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<CompaniesHouseResponse?> GetCompaniesHouseResponseAsync(string id)
+    {
+        var response = await _httpClient.GetAsync($"{CompaniesHouseEndpoint}/{id}");
+        if (response.StatusCode == HttpStatusCode.BadRequest)
+        {
+            var errorResponse = await response.Content.ReadFromJsonAsync<CompaniesHouseErrorResponse>();
+            if (errorResponse?.InnerException?.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<CompaniesHouseResponse>();
+    }
+}
---
+++ src/EPR.SubsidiaryBulkUpload.Application/Services/SubsidiaryService.cs
@@ -0,0 +1,151 @@
+﻿namespace EPR.SubsidiaryBulkUpload.Application.Services;
+using System.Net;
+using System.Net.Http.Json;
+using EPR.SubsidiaryBulkUpload.Application.DTOs;
+using EPR.SubsidiaryBulkUpload.Application.Exceptions;
+using EPR.SubsidiaryBulkUpload.Application.Extensions;
+using Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+public class SubsidiaryService : ISubsidiaryService
+{
+    private const string OrganisationByCompanyHouseNumberUri = "api/organisations/";
+    private const string OrganisationByTableStorageUri = "api/organisations/organisation-by-tablestorage";
+    private const string OrganisationUri = "api/organisations/organisation-by-externalId";
+    private const string OrganisationNameUri = "api/organisations/organisation-by-invite-token";
+    private const string OrganisationCreateAddSubsidiaryUri = "api/organisations/create-and-add-subsidiary";
+    private const string OrganisationAddSubsidiaryUri = "api/organisations/add-subsidiary";
+    private readonly ILogger<SubsidiaryService> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _config;
+
+    public SubsidiaryService(
+        HttpClient httpClient,
+        ILogger<SubsidiaryService> logger,
+        IConfiguration config)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+        _config = config;
+    }
+
+    public async Task<Company?> GetCompanyByOrgId(CompaniesHouseCompany company)
+    {
+        var response = await _httpClient.GetAsync($"{OrganisationByCompanyHouseNumberUri}?organisationId={company.organisation_id}");
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            return null;
+        }
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+            if (problemDetails != null)
+            {
+                throw new ProblemResponseException(problemDetails, response.StatusCode);
+            }
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonWithEnumsAsync<Company>();
+    }
+
+    public async Task<Company?> GetCompanyByCompaniesHouseNumber(string companiesHouseNumber)
+    {
+        var response = await _httpClient.GetAsync($"{OrganisationByCompanyHouseNumberUri}?companiesHouseNumber={companiesHouseNumber}");
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            return null;
+        }
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+            if (problemDetails != null)
+            {
+                throw new ProblemResponseException(problemDetails, response.StatusCode);
+            }
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonWithEnumsAsync<Company>();
+    }
+
+    public async Task<Company> GetCompanyByOrgIdFromTableStorage(string companiesHouseNumber)
+    {
+        var response = await _httpClient.GetAsync($"{OrganisationByTableStorageUri}?companiesHouseNumber={companiesHouseNumber}");
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            return null;
+        }
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+            if (problemDetails != null)
+            {
+                throw new ProblemResponseException(problemDetails, response.StatusCode);
+            }
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonWithEnumsAsync<Company>();
+    }
+
+    public async Task<string?> CreateAndAddSubsidiaryAsync(LinkOrganisationModel linkOrganisationModel)
+    {
+        string json = JsonConvert.SerializeObject(linkOrganisationModel);
+        StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync(OrganisationCreateAddSubsidiaryUri, httpContent);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+            if (problemDetails != null)
+            {
+                throw new ProblemResponseException(problemDetails, response.StatusCode);
+            }
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        return result;
+    }
+
+    public async Task<string?> AddSubsidiaryRelationshipAsync(SubsidiaryAddModel subsidiaryAddModel)
+    {
+        string json = JsonConvert.SerializeObject(subsidiaryAddModel);
+        StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync(OrganisationAddSubsidiaryUri, httpContent);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+            if (problemDetails != null)
+            {
+                throw new ProblemResponseException(problemDetails, response.StatusCode);
+            }
+        }
+
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        return result;
+    }
+}
