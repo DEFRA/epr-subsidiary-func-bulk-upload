47bb28e1ef6acbc80395699f69cc101d7a4dff84
Abid Kayani <abid.kayani.external@eviden.com>
2024-08-06 15:08:05 +0100

    416704: 1.1 Bulk upload processing (Happy path) Interfaces

---
+++ src/EPR.SubsidiaryBulkUpload.Application/Services/Interfaces/ICompaniesHouseLookupService.cs
@@ -0,0 +1,8 @@
+﻿using EPR.SubsidiaryBulkUpload.Application.Models;
+
+namespace EPR.SubsidiaryBulkUpload.Application.Services.Interfaces;
+
+public interface ICompaniesHouseLookupService
+{
+    Task<CompaniesHouseResponse?> GetCompaniesHouseResponseAsync(string id);
+}
---
+++ src/EPR.SubsidiaryBulkUpload.Application/Services/Interfaces/ISubsidiaryService.cs
@@ -0,0 +1,15 @@
+﻿using EPR.SubsidiaryBulkUpload.Application.DTOs;
+
+namespace EPR.SubsidiaryBulkUpload.Application.Services.Interfaces;
+public interface ISubsidiaryService
+{
+    Task<Company?> GetCompanyByCompaniesHouseNumber(string companiesHouseNumber);
+
+    Task<Company?> GetCompanyByOrgId(CompaniesHouseCompany company);
+
+    Task<Company> GetCompanyByOrgIdFromTableStorage(string companiesHouseNumber);
+
+    Task<string?> CreateAndAddSubsidiaryAsync(LinkOrganisationModel linkOrganisationModel);
+
+    Task<string?> AddSubsidiaryRelationshipAsync(SubsidiaryAddModel subsidiaryAddModel);
+}
