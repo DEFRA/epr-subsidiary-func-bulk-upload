6588f04f3dcdbb12dad9f0f68241a5e2d412af6b
Abid Kayani <abid.kayani.external@eviden.com>
2024-08-07 08:58:41 +0100

    416704: Config updates

---
+++ src/EPR.SubsidiaryBulkUpload.Function.UnitTests/BulkUploadFunctionTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using EPR.SubsidiaryBulkUpload.Application.Services.Interfaces;
+using EPR.SubsidiaryBulkUpload.Function.UnitTests.TestHelpers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace EPR.SubsidiaryBulkUpload.Function.UnitTests;
+
+[TestClass]
+public class BulkUploadFunctionTests
+{
+    private const string BlobContainerName = "test_container";
+    private const string CsvBlobName = "test.csv";
+    private const int CsvRowCount = 2;
+    private const string CsvContent =
+        """
+        organisation_id,subsidiary_id,organisation_name,companies_house_number,parent_child,franchisee_licensee_tenant\n
+        100000,,PARENT LIMITED,09910000,Parent,\n
+        100001,SON LTD,19910001,Child,\n
+        100001,12345,DAUGHTER LTD,19910001,Child,\n
+        """;
+
+    private Mock<BlobClient> _blobClientMock;
+    private Mock<ICsvProcessor> _csvProcessorMock;
+    private Mock<ILogger<BulkUploadFunction>> _loggerMock;
+
+    private BulkUploadFunction _systemUnderTest;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _blobClientMock = new Mock<BlobClient>();
+        _blobClientMock
+            .SetupGet(m => m.BlobContainerName)
+            .Returns(BlobContainerName);
+        _blobClientMock
+            .SetupGet(m => m.Name)
+            .Returns(CsvBlobName);
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(CsvContent));
+        var responseMock = new Mock<Response>();
+
+        var downloadStreamingDetails = BlobsModelBuilder.CreateBlobDownloadDetails(
+            CsvContent.Length,
+            new Dictionary<string, string> { { "test", "test" } });
+
+        var downloadStreamingResult = BlobsModelFactory.BlobDownloadStreamingResult(stream, downloadStreamingDetails);
+
+        var response = Response.FromValue(downloadStreamingResult, new Mock<Response>().Object);
+        _blobClientMock.Setup(client => client.DownloadStreamingAsync(It.IsAny<BlobDownloadOptions>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        _csvProcessorMock = new Mock<ICsvProcessor>();
+        _csvProcessorMock.Setup(x => x.ProcessStream(It.IsAny<Stream>()))
+            .ReturnsAsync(CsvRowCount);
+
+        _loggerMock = new Mock<ILogger<BulkUploadFunction>>();
+
+        _systemUnderTest = new BulkUploadFunction(null, null, null, null);
+    }
+
+    [TestMethod]
+    public async Task BulkUploadFunction_Calls_CsvService()
+    {
+        // Act
+        await _systemUnderTest.Run(null, string.Empty);
+
+        // Assert
+        _csvProcessorMock.Verify(x => x.ProcessStream(It.IsAny<Stream>()), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task BulkUploadFunction_Logs_Result()
+    {
+        // Act
+        await _systemUnderTest.Run(null, string.Empty);
+
+        // Assert
+        _loggerMock.VerifyLog(x => x.LogInformation("C# Blob trigger processed {Count} records from csv blob {Name}", CsvRowCount, CsvBlobName), Times.Once);
+    }
+}
--- src/EPR.SubsidiaryBulkUpload.Function/Extensions/ConfigurationExtensions.cs
+++ src/EPR.SubsidiaryBulkUpload.Function/Extensions/ConfigurationExtensions.cs
@@ -1,10 +1,15 @@
 ﻿namespace EPR.SubsidiaryBulkUpload.Function.Extensions;
 
 using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using EPR.SubsidiaryBulkUpload.Application.Configs;
 using EPR.SubsidiaryBulkUpload.Application.Services;
 using EPR.SubsidiaryBulkUpload.Application.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 [ExcludeFromCodeCoverage]
 public static class ConfigurationExtensions
@@ -14,7 +19,6 @@ public static class ConfigurationExtensions
         /*
         services.Configure<AntivirusApiOptions>(configuration.GetSection(AntivirusApiOptions.Section));
         */
-
         return services;
     }
 
@@ -34,7 +38,7 @@ public static class ConfigurationExtensions
         return services;
     }
 
-    public static IServiceCollection AddHttpClients(this IServiceCollection services)
+    public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
     {
         // var sp = services.BuildServiceProvider();
 
@@ -58,13 +62,72 @@ public static class ConfigurationExtensions
         }).AddHttpMessageHandler<TradeAntivirusApiAuthorizationHandler>();
         */
 
+        /*
+        services.AddTransient<AccountServiceAuthorisationHandler>();
+
+        services.AddHttpClient<ISubsidiaryService, SubsidiaryService>((sp, client) =>
+        {
+            var config = sp.GetRequiredService<IOptions<ApiConfig>>().Value;
+
+            client.BaseAddress = new Uri(config.SubsidiaryServiceBaseUrl);
+            client.Timeout = TimeSpan.FromSeconds(config.Timeout);
+        })
+        .AddHttpMessageHandler(AccountServiceAuthorisationHandler);
+        */
+
+        services.AddHttpClient<ISubsidiaryService, SubsidiaryService>((sp, c) =>
+        {
+            var config = sp.GetRequiredService<IOptions<ApiConfig>>().Value;
+            c.BaseAddress = new Uri(config.SubsidiaryServiceBaseUrl);
+            c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        });
+
+       /*     const string CompaniesHouseClient = "CompaniesHouse";
+            const string CompaniesHouseBaseUri = "https://api.company-information.service.gov.uk/";  // configuration.GetValue<string>("CompaniesHouseApi__BaseUri");
+            const string CompaniesHouseApiKey = ""; // configuration.GetSection("CompaniesHouseApi.ApiKey").Value;
+
+            services.AddHttpClient(CompaniesHouseClient, client =>
+            {
+                client.BaseAddress = new Uri(CompaniesHouseBaseUri);
+                var apiKey = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{CompaniesHouseApiKey}:"));
+                client.DefaultRequestHeaders.Add("Authorization", $"BASIC {apiKey}");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+
+            services.AddHttpClient<ICompaniesHouseLookupService, CompaniesHouseLookupService>((sp, client) =>
+            {
+                var config = sp.GetRequiredService<IOptions<ApiConfig>>().Value;
+
+                client.BaseAddress = new Uri(config.CompaniesHouseLookupBaseUrl);
+                client.Timeout = TimeSpan.FromSeconds(config.Timeout);
+            })
+            .ConfigurePrimaryHttpMessageHandler(GetClientCertificateHandler);*/
+
         return services;
     }
 
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
         services.AddTransient<ICsvProcessor, CsvProcessor>();
-
+        services.AddTransient<ICompaniesHouseLookupService, CompaniesHouseLookupService>();
+        services.AddTransient<ISubsidiaryService, SubsidiaryService>();
         return services;
+    }
+
+    private static HttpMessageHandler GetClientCertificateHandler(IServiceProvider sp)
+    {
+        if (sp == null)
+        {
+            throw new ArgumentException("ServiceProvider must not be null");
+        }
+
+        var handler = new HttpClientHandler();
+        handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+        handler.SslProtocols = SslProtocols.Tls12;
+        handler.ClientCertificates
+        .Add(new X509Certificate2(
+        Convert.FromBase64String(sp.GetRequiredService<IOptions<ApiConfig>>().Value.Certificate)));
+
+        return handler;
     }
 }
