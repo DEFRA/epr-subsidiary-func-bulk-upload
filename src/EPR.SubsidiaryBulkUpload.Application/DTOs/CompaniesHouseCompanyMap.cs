7d834f384881717f37c10ddf64598eaf9334755a
Abid Kayani <abid.kayani.external@eviden.com>
2024-09-13 10:29:48 +0100

    code refactoring

--- src/EPR.SubsidiaryBulkUpload.Application/DTOs/CompaniesHouseCompanyMap.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/DTOs/CompaniesHouseCompanyMap.cs
@@ -1,9 +1,11 @@
-﻿using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using CsvHelper.Configuration;
 using EPR.SubsidiaryBulkUpload.Application.Models;
 
 namespace EPR.SubsidiaryBulkUpload.Application.DTOs;
 
+[ExcludeFromCodeCoverage]
 public class CompaniesHouseCompanyMap : ClassMap<CompaniesHouseCompany>
 {
     public CompaniesHouseCompanyMap()
