1befd7212e3cabe29ceaf8559ae98a18bfcfefbd
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2024-09-30 17:19:16 +0100

    Allow for not found and internal service error from companies house api

--- src/EPR.SubsidiaryBulkUpload.Application/Services/CompaniesHouseLookupDirectService.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/Services/CompaniesHouseLookupDirectService.cs
@@ -19,7 +19,9 @@ public class CompaniesHouseLookupDirectService : ICompaniesHouseLookupService
     public async Task<Company?> GetCompaniesHouseResponseAsync(string id)
     {
         var response = await _httpClient.GetAsync($"{CompaniesHouseEndpoint}/{id}");
-        if (response.StatusCode == HttpStatusCode.NoContent)
+        if (response.StatusCode == HttpStatusCode.NoContent ||
+            response.StatusCode == HttpStatusCode.NotFound ||
+            response.StatusCode == HttpStatusCode.InternalServerError)
         {
             return null;
         }
