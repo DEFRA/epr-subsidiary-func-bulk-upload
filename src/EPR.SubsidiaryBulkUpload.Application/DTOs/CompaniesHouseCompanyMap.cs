cfc846c708f70769c5b21d81b986848fd698f841
Abid Kayani <abid.kayani.external@eviden.com>
2024-10-08 09:29:21 +0100

    439650: white space checks on companies house number

--- src/EPR.SubsidiaryBulkUpload.Application/DTOs/CompaniesHouseCompanyMap.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/DTOs/CompaniesHouseCompanyMap.cs
@@ -42,23 +42,21 @@ public class CompaniesHouseCompanyMap : ClassMap<CompaniesHouseCompany>
                     lineNumber, rawData, BulkUpdateErrors.OrganisationNameRequiredMessage, BulkUpdateErrors.OrganisationNameRequired));
         }
 
-        if (string.IsNullOrEmpty(row.GetField(nameof(CompaniesHouseCompany.companies_house_number)))
-            && string.IsNullOrEmpty(row.GetField(nameof(CompaniesHouseCompany.franchisee_licensee_tenant))))
+        if (string.IsNullOrEmpty(row.GetField(nameof(CompaniesHouseCompany.companies_house_number))) && string.IsNullOrEmpty(row.GetField(nameof(CompaniesHouseCompany.franchisee_licensee_tenant))))
         {
             errors.Add(
                 CreateError(
                     lineNumber, rawData, BulkUpdateErrors.CompaniesHouseNumberRequiredMessage, BulkUpdateErrors.CompaniesHouseNumberRequired));
         }
 
-        if (!string.IsNullOrEmpty(row.GetField(nameof(CompaniesHouseCompany.companies_house_number)))
-            && row.GetField(nameof(CompaniesHouseCompany.companies_house_number)).Length > 8)
+        if (!string.IsNullOrEmpty(row.GetField(nameof(CompaniesHouseCompany.companies_house_number))) && row.GetField(nameof(CompaniesHouseCompany.companies_house_number)).Length > 8)
         {
             errors.Add(
                 CreateError(
                     lineNumber, rawData, BulkUpdateErrors.InvalidCompaniesHouseNumberLengthErrorMessage, BulkUpdateErrors.InvalidCompaniesHouseNumberLengthError));
         }
 
-        if (!string.IsNullOrWhiteSpace(row.GetField(nameof(CompaniesHouseCompany.companies_house_number))))
+        if (string.IsNullOrWhiteSpace(row.GetField(nameof(CompaniesHouseCompany.companies_house_number))) && row.GetField(nameof(CompaniesHouseCompany.companies_house_number)).Contains(" "))
         {
             errors.Add(
                 CreateError(
