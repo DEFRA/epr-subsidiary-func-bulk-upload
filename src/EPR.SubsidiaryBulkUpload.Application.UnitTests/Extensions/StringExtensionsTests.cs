64d5e246e562ec63f26bbe687be5eaebee6fc573
Mike Wild <40885416+mikeymonster@users.noreply.github.com>
2024-10-11 14:07:47 +0100

    AB#430328 multi file companies house data ingestion (#30)
    * Happy path companies house download.
    Added file download and download storage status and the happy path companies house download
    
    * Added download status storage edge case unit tests
    
    * Get companies house download from configuration
    
    * Pulled in antivirus client from WebApiGateway
    
    * Pulled in the anti virus client from WebApiGateway
    
    * Post a file stream to antivirus
    Changed imported API's to not throw everywhere!
    Test coverage for imported API's
    
    * Lost a change!!!
    
    * ... and its tesst :)
    
    * Added Http clients
    
    * Added registrations
    
    * Fixed SonarQube issue
    
    * More SonarQube issues
    
    * SonarQube issues
    
    * SonarQube
    
    * SonarQube
    
    * Corrected download file name and configuration options
    
    * Fixed error message typo
    
    * Slipped the submissions API options to what was already in config
    
    * Removed local settings reference from project
    
    * Somehow the notification function was removed. Dont know how, noit seen that before :)
    
    * Removed guids from readme.md for gitleaks scan. They are not a leak!!
    
    * Still thinks there is a leak. Not sure why :(
    
    * Ignored an invalid gitleak :)
    
    * Add calls and provider to get system user and org
    
    * Change subsidiary service to not throw exception
    
    * Use lazy initialization on system details and make it scoped
    
    * Use new system details provider instead of config
    
    * Improved loging
    
    * Fixed logging test
    
    * Post-merge fixes to ConfigurationExtensions
    
    * Undo sonarqube "fix" and wrap with pragmas to warn people
    
    Making a string static caused problems with serializing FileDetails
    and meant antivirus always returned Bad Request
    
    * Suppress excessive Azure.Core blob storage logging
    
    * Add AntivirusApiOptions NotificationEmail
    
    * Remove ApiOptions system userr/org id
    
    * Make system user/org nullable and refactor
    
     - added MissingSystemDetailsException but made sure to catch it i submissionStatusClient
     - logged missing system user id in CompaniesHouseFilePostService.cs
     - fixed a couple of typos
    
    * Remove commented line missed on last checkin
    
    * Sonar fix - change .All to Arry.TrueForAll
    
    * Post-merge fixes
    
    * Allow for not found and internal service error from companies house api
    
    * Don't run download on startup
    
    * Fix casing on test name
    
    * Minor fixes, add httpClient for file download
    
    * Get download file from ZipArchive
    
    * Revert "Don't delete blob while download is tested"
    
    This reverts commit 57be6fd611cc93533f902b08e883135f53877c71.
    
    * Post-merge fixes to test
    
    * Use correct stream from zip file
    
    * Remove unused code and other tidying
    
    * Add extension to extract file parts from file name
    
    * Move suppress SA1010 to Directory.Build.props
    
    * Implement two-layer concurrency
    
     - not working well, will attempt to refactor
    
    * Simplify by removing ETag code
    
    * Refactoring as suggested by sonarqube
    
    * Added a couple of tests
    
    * Post-merge fixes
    
    * Add tests for larger batches of records
    
    ---------
    
    Co-authored-by: BrianKnightEviden <155995473+BrianKnightEviden@users.noreply.github.com>
    Co-authored-by: Mike Wild <Michael.Wild@defra.onmicrosoft.com>

--- src/EPR.SubsidiaryBulkUpload.Application.UnitTests/Extensions/StringExtensionsTests.cs
+++ src/EPR.SubsidiaryBulkUpload.Application.UnitTests/Extensions/StringExtensionsTests.cs
@@ -10,22 +10,27 @@ public class StringExtensionsTests
     [DataRow("", "")]
     [DataRow("CompanyDataFile20240801.csv", "")]
     [DataRow("CompanyDataFile-2024-08-01.csv", "2024-08-01")]
-    public void ToFindPartitionKey_ShouldFormatCorrectly(string input, string expectedResult)
+    [DataRow("BasicCompanyData-2024-10-01-part1_3.zip", "2024-10-01")]
+    public void ToPartitionKey_ShouldFormatCorrectly(string input, string expectedResult)
     {
-        var result = input.ToFindPartitionKey();
+        var result = input.ToPartitionKey();
 
         result.Should().Be(expectedResult);
     }
 
     [TestMethod]
-    [DataRow(null, "")]
-    [DataRow("", "")]
-    [DataRow("CompanyDataFile.csv", "")]
-    [DataRow("CompanyDataFile-2024-07-01.csv", "2024-07-01")]
-    public void ToPartitionKeyFormat_ShouldFormatCorrectly(string input, string expectedResult)
+    [DataRow(null, 0, 0)]
+    [DataRow("", 0, 0)]
+    [DataRow("CompanyDataFile-2024-08-01.csv", 0, 0)]
+    [DataRow("BasicCompanyData-2024-12-01-part18.zip", 0, 0)]
+    [DataRow("BasicCompanyData-2024-12-01-part1_8.zip", 1, 8)]
+    [DataRow("BasicCompanyData-2024-12-01-part5_10.zip", 5, 10)]
+    public void ToFilePartNumberAndCounty_ShouldFormatCorrectly(string input, int expectedFilePart, int expectedFileCount)
     {
-        var result = input.ToPartitionKeyFormat();
+        var result = input.ToFilePartNumberAndCount();
 
-        result.Should().Be(expectedResult);
+        result.Should().NotBeNull();
+        result.PartNumber.Should().Be(expectedFilePart);
+        result.TotalFiles.Should().Be(expectedFileCount);
     }
 }
