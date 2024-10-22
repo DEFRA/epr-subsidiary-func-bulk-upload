9cc2908907fb537ff28c219ad48714c8296a8c41
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2024-10-22 11:55:16 +0100

    Add another companies house download setting

--- src/EPR.SubsidiaryBulkUpload.Application/Options/CompaniesHouseDownloadOptions.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/Options/CompaniesHouseDownloadOptions.cs
@@ -8,4 +8,6 @@ public class CompaniesHouseDownloadOptions : ApiResilienceOptions
     public const string SectionName = "CompaniesHouseDownload";
 
     public string CompaniesHouseDataDownloadUrl { get; set; } = null!;
+
+    public string CompaniesHouseFileDownloadPage { get; set; } = null!;
 }
