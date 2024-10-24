5e8ba58b34cbc8e3b8377d1ac62e105d7bec25a0
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2024-10-24 19:27:33 +0100

    Add test to prove fix on Outcome.Response.StatusCode
     - the code was sometimes throwing null reference exceptions. That should no longer happen.

--- src/EPR.SubsidiaryBulkUpload.Application.UnitTests/Resilience/PipelineTests.cs
+++ src/EPR.SubsidiaryBulkUpload.Application.UnitTests/Resilience/PipelineTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using EPR.SubsidiaryBulkUpload.Application.Exceptions;
 using EPR.SubsidiaryBulkUpload.Application.Models;
 using EPR.SubsidiaryBulkUpload.Application.Options;
 using EPR.SubsidiaryBulkUpload.Application.Resilience;
@@ -174,6 +175,54 @@ public class PipelineTests
 
         // Assert
         attempts.Should().Be(MaxRetries + 1);
+    }
+
+    [TestMethod]
+    public async Task CompaniesHouseResilienceHandlerTest_For_Exception_Should_Not_Retry_Or_Fail()
+    {
+        // Arrange
+        var attempts = 0;
+
+        var services = new ServiceCollection();
+        var exception = new NotSupportedException("This should be caught, not a different exception thrown inside the pipeline.");
+
+        services.Configure<ApiOptions>(x =>
+        {
+            x.CompaniesHouseLookupBaseUrl = BaseAddress;
+            x.RetryPolicyInitialWaitTime = 1;
+            x.RetryPolicyMaxRetries = MaxRetries;
+            x.RetryPolicyTooManyAttemptsWaitTime = 1;
+            x.RetryPolicyTooManyAttemptsMaxRetries = MaxRetriesFor429;
+            x.Timeout = 10; // Minimum value 10ms required for this
+            x.TimeUnits = TimeUnit.Milliseconds;
+        });
+
+        _httpMessageHandlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(() => { throw exception; })
+            .Callback<HttpRequestMessage, CancellationToken>((m, c) => attempts++);
+
+        services.AddHttpClient(HttpClientName, client =>
+        {
+            client.BaseAddress = new Uri(BaseAddress);
+        })
+            .AddCompaniesHouseResilienceHandlerToHttpClientBuilder()
+            .AddHttpMessageHandler(() => _httpMessageHandlerMock.Object);
+
+        var serviceProvider = services.BuildServiceProvider();
+        using var scope = serviceProvider.CreateScope();
+
+        var sut = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient(HttpClientName);
+        var request = new HttpRequestMessage(HttpMethod.Get, "/any");
+
+        // Act
+        await Assert.ThrowsExceptionAsync<NotSupportedException>(async () => await sut.SendAsync(request));
+
+        // Assert
+        attempts.Should().Be(0);
     }
 
     [TestMethod]
