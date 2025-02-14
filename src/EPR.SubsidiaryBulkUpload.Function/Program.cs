5f8ec755a561c49a31435d4e4d4e44c206b05974
Mike Wild <40885416+mikeymonster@users.noreply.github.com>
2025-02-14 09:22:28 +0000

    Cherry pick release 8.0 Boomi changes (#64)
    Migrate companies house api to oauth (#59) (#62)
    
    * Migrate companies house api to oauth
    
    * Add feature flag to use certificate or OAuth for Boomi
    
    ---------
    
    Co-authored-by: Mike Wild <Michael.Wild@defra.onmicrosoft.com>

--- src/EPR.SubsidiaryBulkUpload.Function/Program.cs
+++ src/EPR.SubsidiaryBulkUpload.Function/Program.cs
@@ -2,11 +2,14 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices((hostingContext, services) =>
     {
+        services.AddFeatureManagement();
+
         services.AddApplicationInsightsTelemetryWorkerService()
             .ConfigureFunctionsApplicationInsights()
             .ConfigureOptions(hostingContext.Configuration)
