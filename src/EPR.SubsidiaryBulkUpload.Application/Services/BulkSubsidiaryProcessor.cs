8d66c94616acee22abd97a05ac3d0c2beeeabcf9
Abid Kayani <abid.kayani.external@eviden.com>
2025-02-20 13:53:45 +0000

    500402: varible removed

--- src/EPR.SubsidiaryBulkUpload.Application/Services/BulkSubsidiaryProcessor.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/Services/BulkSubsidiaryProcessor.cs
@@ -47,7 +47,7 @@ public class BulkSubsidiaryProcessor(ISubsidiaryService organisationService, ICo
         && string.Equals(sub.Subsidiary.organisation_name, sub.SubsidiaryOrg.name, StringComparison.OrdinalIgnoreCase)
         && parentOrg.id == sub.SubsidiaryOrg.OrganisationRelationship?.FirstOrganisationId);
 
-        var subsidiariesAndOrgWithValidNameanJointerDateProcessStatistics = await ProcessValidNamedOrgsUpdate(subsidiariesAndOrg, parentOrg, userRequestModel);
+        var subsidiariesAndOrgWithValidNameanJointerDateProcessStatistics = await ProcessValidNamedOrgsUpdate(subsidiariesAndOrgWithMatchingReportingType, parentOrg, userRequestModel);
 
         var subsidiariesAndOrgWith_InValidName = subsidiariesAndOrg.Where(sub => sub.Subsidiary.companies_house_number == sub.SubsidiaryOrg?.companiesHouseNumber
             && !string.Equals(sub.Subsidiary.organisation_name, sub.SubsidiaryOrg?.name, StringComparison.OrdinalIgnoreCase));
