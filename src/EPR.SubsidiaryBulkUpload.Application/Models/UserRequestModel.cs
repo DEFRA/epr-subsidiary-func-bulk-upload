872a4970c7e29d10e1622d8fbe1cc4457d655d01
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-02-24 14:00:32 +0000

    Remove unused field and reorder UserRequestModel fields

--- src/EPR.SubsidiaryBulkUpload.Application/Models/UserRequestModel.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/Models/UserRequestModel.cs
@@ -5,19 +5,17 @@ namespace EPR.SubsidiaryBulkUpload.Application.Models;
 [ExcludeFromCodeCoverage]
 public class UserRequestModel
 {
-    public Guid UserId { get; set; }
-
-    public Guid OrganisationId { get; set; }
-
-    public Guid? ComplianceSchemeId { get; set; }
-
     public string? BlobName { get; set; }
 
     public string? BlobContainerName { get; set; }
 
+    public Guid? ComplianceSchemeId { get; set; }
+
     public string? FileName { get; set; }
+
+    public Guid OrganisationId { get; set; }
 
     public Guid? SubmissionId { get; set; }
 
-    public bool? IncludeSubsidiaryJoinerColumns { get; set; }
+    public Guid UserId { get; set; }
 }
