8d83a9c4bc153b759d1c90ab81f468f209572d75
Kader Hashmi (ext) <kader.hashmi.external@eviden.com>
2025-05-22 10:20:53 +0100

    Revert "Add Sync Subs barebones function"
    This reverts commit 850c5ebe6bd268faddf98e708c4e483a34c77375.

--- src/EPR.SubsidiaryBulkUpload.Application/Constants/FeatureFlags.cs
+++ src/EPR.SubsidiaryBulkUpload.Application/Constants/FeatureFlags.cs
@@ -4,5 +4,4 @@ public static class FeatureFlags
 {
     public const string EnableSubsidiaryJoinerColumns = "EnableSubsidiaryUploadJoinerColumns";
     public const string EnableSubsidiaryNationColumn = "EnableSubsidiaryNationColumn";
-    public const string EnableSyncSubsidiariesFromRegistrationFile = "EnableSyncSubsidiariesFromRegistrationFile";
 }
