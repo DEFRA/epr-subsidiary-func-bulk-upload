26ea6717a9d4f805c8d2f3b9c1cbd9ca1e094fdf
Kader Hashmi (ext) <kader.hashmi.external@eviden.com>
2025-05-22 10:28:11 +0100

    Add Sync Subs barebones function

---
+++ src/EPR.SubsidiaryBulkUpload.Function/SyncSubsidiariesFromRegistrationFileFunction.cs
@@ -0,0 +1,28 @@
+﻿using EPR.SubsidiaryBulkUpload.Application.Constants;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace EPR.SubsidiaryBulkUpload.Function;
+
+public class SyncSubsidiariesFromRegistrationFileFunction(IFeatureManager featureManager, ILogger<SyncSubsidiariesFromRegistrationFileFunction> logger)
+{
+    private const string LogPrefix = nameof(SyncSubsidiariesFromRegistrationFileFunction);
+
+    [Function("SyncSubsidiariesFromRegistrationFileFunction")]
+    public async Task Run([TimerTrigger("%SyncSubsidiariesFromRegistrationFile:Schedule%", RunOnStartup = true)] TimerInfo timerInfo)
+    {
+        if (!await featureManager.IsEnabledAsync(FeatureFlags.EnableSyncSubsidiariesFromRegistrationFile))
+        {
+            logger.LogInformation("{LogPrefix} Function is disabled by feature flag.", LogPrefix);
+            return;
+        }
+
+        logger.LogInformation("{LogPrefix} Starting Sync Subsidiaries from Registration File to Accounts DB at {ExecutionTime}", LogPrefix, DateTime.Now);
+
+        if (timerInfo.ScheduleStatus?.Next is not null)
+        {
+            logger.LogInformation("{LogPrefix} Next Sync Subsidiaries job scheduled at {NextTime}", LogPrefix, timerInfo.ScheduleStatus.Next);
+        }
+    }
+}
